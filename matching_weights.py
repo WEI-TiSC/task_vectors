79e9f97c3b9a395783d5810fc03cf48e42617a7b
I Kungo <81465578+AvavaAva@users.noreply.github.com>
2024-11-10 14:28:18 +0900

    Modify arg_parser

--- matching_weights.py
+++ matching_weights.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import torch
 from jax import random
@@ -5,42 +7,6 @@ from jax import random
 import src.weight_matching as wm
 from src.args import parse_arguments
 
-# # Configs
-# victim_task = 'EuroSAT'
-# free_rider_task = 'DTD'
-# model = 'ViT-B-32'
-# vector_scaling_coef = 0.3
-#
-# args = parse_arguments()
-# args.data_location = 'data'
-# args.model = model
-# args.save = f'checkpoints/{model}'
-#
-# victim_task_checkpoint = f'checkpoints/{model}/{victim_task}/finetuned.pt'  # Vector to be merged...
-# pretrained_checkpoint = f'checkpoints/{model}/zeroshot.pt'  # Pre-trained checkpoint for T_source
-# free_rider_task_checkpoint = f'checkpoints/{model}/{free_rider_task}/finetuned.pt'  # Theta_dest, who wants T_source
-#
-# victim_encoder = torch.load(victim_task_checkpoint)
-# free_rider_encoder = torch.load(free_rider_task_checkpoint)
-#
-# victim_params = {name: param.clone() for name, param in victim_encoder.state_dict().items() if 'mlp.c' in name}
-# fr_params = {name: param.clone() for name, param in free_rider_encoder.state_dict().items() if 'mlp.c' in name}
-#
-# # Weight matching
-# perm_spec = wm.vit_b_32_permutation_spec_MLP(num_layers=12) # Do all layers but only MLP
-# rng = random.PRNGKey(0)
-# permutation, _, _, _ = wm.weight_matching(rng, perm_spec, victim_params, fr_params)
-# permuted_victim_MLP_params = {k: torch.tensor(np.array(v)) for k, v in
-#                           wm.apply_permutation(perm_spec, permutation, victim_params).items()}
-#
-# full_victim_params = {name: param.clone() for name, param in victim_encoder.state_dict().items()}
-# full_victim_params.update(permuted_victim_MLP_params)
-#
-# victim_encoder.load_state_dict(full_victim_params)
-# permuted_victim_params = {name: param.clone() for name, param in victim_encoder.state_dict().items() if 'mlp.c' in name}
-#
-# torch.save(victim_encoder, f'permuted models/white box/{model}/{victim_task}/'
-#                            f'victim_{victim_task}_fr_{free_rider_task}_permuted.pt')
 
 if __name__ == "__main__":
     args = parse_arguments()
@@ -85,7 +51,10 @@ if __name__ == "__main__":
         assert torch.ne(permuted_victim_params[layer],
                         victim_params[layer]).any(), f"Tensors are equal for layer: {layer}"
 
-    save_path = f'permuted models/white box/{model}/{victim_task}/victim_{victim_task}_fr_{free_rider_task}_permuted.pt'
-    torch.save(victim_encoder, save_path)
-    print(f"Model saved to {save_path}")
+    save_path = f'permuted models/white box/{model}/{victim_task}/'
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    model_name = f'victim_{victim_task}_fr_{free_rider_task}_permuted.pt'
+    save_model = os.path.join(save_path, model_name)
+    torch.save(victim_encoder, save_model)
+    print(f"Model saved to {save_model}")
 
---
+++ run_permutation_vit_b_16.sh
@@ -0,0 +1,55 @@
+# RESISC45
+python matching_weights.py --victim_task RESISC45 --free_rider_task MNIST --base_model ViT-B-16
+python matching_weights.py --victim_task RESISC45 --free_rider_task SVHN --base_model ViT-B-16
+python matching_weights.py --victim_task RESISC45 --free_rider_task DTD --base_model ViT-B-16
+python matching_weights.py --victim_task RESISC45 --free_rider_task EuroSAT --base_model ViT-B-16
+python matching_weights.py --victim_task RESISC45 --free_rider_task Cars --base_model ViT-B-16
+python matching_weights.py --victim_task RESISC45 --free_rider_task GTSRB --base_model ViT-B-16
+
+## GTSRB
+python matching_weights.py --victim_task GTSRB --free_rider_task MNIST --base_model ViT-B-16
+python matching_weights.py --victim_task GTSRB --free_rider_task SVHN --base_model ViT-B-16
+python matching_weights.py --victim_task GTSRB --free_rider_task DTD --base_model ViT-B-16
+python matching_weights.py --victim_task GTSRB --free_rider_task EuroSAT --base_model ViT-B-16
+python matching_weights.py --victim_task GTSRB --free_rider_task Cars --base_model ViT-B-16
+python matching_weights.py --victim_task GTSRB --free_rider_task RESISC45 --base_model ViT-B-16
+
+# Cars
+python matching_weights.py --victim_task Cars --free_rider_task MNIST --base_model ViT-B-16
+python matching_weights.py --victim_task Cars --free_rider_task SVHN --base_model ViT-B-16
+python matching_weights.py --victim_task Cars --free_rider_task DTD --base_model ViT-B-16
+python matching_weights.py --victim_task Cars --free_rider_task EuroSAT --base_model ViT-B-16
+python matching_weights.py --victim_task Cars --free_rider_task RESISC45 --base_model ViT-B-16
+python matching_weights.py --victim_task Cars --free_rider_task GTSRB --base_model ViT-B-16
+
+# DTD
+python matching_weights.py --victim_task DTD --free_rider_task MNIST --base_model ViT-B-16
+python matching_weights.py --victim_task DTD --free_rider_task SVHN --base_model ViT-B-16
+python matching_weights.py --victim_task DTD --free_rider_task RESISC45 --base_model ViT-B-16
+python matching_weights.py --victim_task DTD --free_rider_task EuroSAT --base_model ViT-B-16
+python matching_weights.py --victim_task DTD --free_rider_task Cars --base_model ViT-B-16
+python matching_weights.py --victim_task DTD --free_rider_task GTSRB --base_model ViT-B-16
+
+## MNIST
+python matching_weights.py --victim_task MNIST --free_rider_task GTSRB --base_model ViT-B-16
+python matching_weights.py --victim_task MNIST --free_rider_task SVHN --base_model ViT-B-16
+python matching_weights.py --victim_task MNIST --free_rider_task DTD --base_model ViT-B-16
+python matching_weights.py --victim_task MNIST --free_rider_task EuroSAT --base_model ViT-B-16
+python matching_weights.py --victim_task MNIST --free_rider_task Cars --base_model ViT-B-16
+python matching_weights.py --victim_task MNIST --free_rider_task RESISC45 --base_model ViT-B-16
+
+# SVHN
+python matching_weights.py --victim_task SVHN --free_rider_task MNIST --base_model ViT-B-16
+python matching_weights.py --victim_task SVHN --free_rider_task Cars --base_model ViT-B-16
+python matching_weights.py --victim_task SVHN --free_rider_task DTD --base_model ViT-B-16
+python matching_weights.py --victim_task SVHN --free_rider_task EuroSAT --base_model ViT-B-16
+python matching_weights.py --victim_task SVHN --free_rider_task RESISC45 --base_model ViT-B-16
+python matching_weights.py --victim_task SVHN --free_rider_task GTSRB --base_model ViT-B-16
+
+# EuroSAT
+python matching_weights.py --victim_task EuroSAT --free_rider_task MNIST --base_model ViT-B-16
+python matching_weights.py --victim_task EuroSAT --free_rider_task SVHN --base_model ViT-B-16
+python matching_weights.py --victim_task EuroSAT --free_rider_task DTD --base_model ViT-B-16
+python matching_weights.py --victim_task EuroSAT --free_rider_task Cars --base_model ViT-B-16
+python matching_weights.py --victim_task EuroSAT --free_rider_task RESISC45 --base_model ViT-B-16
+python matching_weights.py --victim_task EuroSAT --free_rider_task GTSRB --base_model ViT-B-16
