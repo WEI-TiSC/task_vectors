1d370d257e70f0025ee704e1b4779ca49ab0efb4
I Kungo <81465578+AvavaAva@users.noreply.github.com>
2024-11-19 23:09:33 +0900

    UPDATE

--- partial_permute.sh
+++ partial_permute.sh
@@ -16,12 +16,16 @@ python experiments/partial_permutation/partial_perms_black_box.py --victim_task 
 
 # 2 layers
 python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [0,1]
-python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [5,6]
+python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [2,3]
+python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [4,5]
+python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [6,7]
+python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [8,9]
 python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [10,11]
 
 # 3 layers
 python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [0,1,2]
-python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [5,6,7]
+python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [3,4,5]
+python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [6,7,8]
 python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [9,10,11]
 
 # 4 layers
@@ -32,7 +36,7 @@ python experiments/partial_permutation/partial_perms_black_box.py --victim_task 
 # 5 layers
 python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [0,1,2,3,4]
 python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [4,5,6,7,8]
-python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [8,9,10,11,12]
+python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [7,8,9,10,11]
 
 # 6 layers
 python experiments/partial_permutation/partial_perms_black_box.py --victim_task MNIST --base_model ViT-B-32 --perm_layers [0,1,2,3,4,5]
